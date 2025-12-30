e03929acd15857d73bb50b78c63a1f0600bbd890
IndulaH <anupaindula50@gmail.com>
2025-12-30 19:53:12 +0530

    breadcrumb and json input

--- src/app/(dashboard)/job-ticket/page.tsx
+++ src/app/(dashboard)/job-ticket/page.tsx
@@ -1,9 +1,18 @@
+import PageTitleWithBreadcrumb from "@/common/PageTitileWithBreadCrumb";
 import React from "react";
 
 function JobTicketComponent() {
-    return <div>
-
-        I am Job Ticket Management</div>;
+    return (
+        <div className="flex flex-1 flex-col gap-4 p-[24px] pt-0 mt-3">
+            <PageTitleWithBreadcrumb
+                title="Job Ticket Management"
+                breadcrumbs={[
+                    { title: "Dashboard", href: "/dashboard" }
+                ]}
+            />
+            <div>I am Job Ticket Management</div>
+        </div>
+    );
 }
 
 export default JobTicketComponent;
