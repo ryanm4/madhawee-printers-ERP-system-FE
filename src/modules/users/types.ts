c7296915708c14eb8c1e333e42bfe46772bf7e2c
IndulaH <anupaindula50@gmail.com>
2026-06-02 11:35:10 +0530

    changes

--- src/modules/users/types.ts
+++ src/modules/users/types.ts
@@ -1,7 +1,7 @@
 export interface GET_ALL_USER {
   id: number;
   name: string;
-  email: string;
+  email?: string;
   user_role: string;
   created_on: string;
   updated_on: string;
@@ -9,13 +9,13 @@ export interface GET_ALL_USER {
 
 export interface CREATE_USER {
   name: string;
-  email: string;
+  email?: string;
   user_role: string;
   password: string;
 }
 
 export interface EDIT_USER {
   name: string;
-  email: string;
+  email?: string;
   user_role: string;
 }
